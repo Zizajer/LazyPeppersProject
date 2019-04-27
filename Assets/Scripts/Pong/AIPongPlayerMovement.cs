f04b08ab2c3be828930f85ebc71148cc7dda0ae8
Piotr <zizi5673@gmail.com>
2019-04-27 09:31:37 +0200

    Added fully operational Pong Prototype.

---
+++ Assets/Scripts/Pong/AIPongPlayerMovement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AIPongPlayerMovement : MonoBehaviour
+{
+    public GameObject Ball;
+    [Range(0, 1)]
+    public float Skill;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        Vector2 newPosition = transform.position;
+        newPosition.y = Mathf.Lerp(transform.position.y, Ball.transform.position.y, Skill);
+        transform.position = newPosition;
+    }
+
+    void FixedUpdate()
+    {
+        
+    }
+}
--- Assets/Scripts/Pong/Ball.cs
+++ Assets/Scripts/Pong/Ball.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class Ball : MonoBehaviour
 {
     private Rigidbody2D rigidbody;
-    public float InitialForce;
+    public float BallSpeed;
 
     // Start is called before the first frame update
     void Start()
@@ -17,11 +17,44 @@ public class Ball : MonoBehaviour
 
     void InitOnPlayerSide()
     {
-        rigidbody.AddForce(Vector2.left * new Vector2(0 , Random.Range(0,180))  * InitialForce);
+        rigidbody.AddForce(Vector2.left  * BallSpeed);
     }
 
     void InitOnAIPlayerSide()
     {
-        rigidbody.AddForce(Vector2.right * InitialForce);
+        rigidbody.AddForce(Vector2.right * BallSpeed);
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "PongPlayer")
+        {
+            float y = hitFactor(transform.position,
+                            collision.transform.position,
+                            collision.bounds.size.y);
+
+            // Calculate direction, make length=1 via .normalized
+            Vector2 dir = new Vector2(-1, y).normalized;
+
+            GetComponent<Rigidbody2D>().velocity = dir * BallSpeed;
+        }
+        if(collision.tag == "AIPongPlayer")
+        {
+            float y = hitFactor(transform.position,
+                            collision.transform.position,
+                            collision.bounds.size.y);
+
+            // Calculate direction, make length=1 via .normalized
+            Vector2 dir = new Vector2(1, y).normalized;
+            
+            GetComponent<Rigidbody2D>().velocity = dir * BallSpeed;
+        }
+    }
+
+    float hitFactor(Vector2 ballPos, Vector2 racketPos,
+                float racketHeight)
+    {
+        return (ballPos.y - racketPos.y) / racketHeight;
     }
 }
