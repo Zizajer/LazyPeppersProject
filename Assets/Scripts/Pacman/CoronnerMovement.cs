7d6e1b39b4cea8c255718a93b803926783fc0be7
Piotr <zizi5673@gmail.com>
2019-04-28 03:16:20 +0200

    Added pacman functionality.

---
+++ Assets/Scripts/Pacman/CoronnerMovement.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoronnnerMovement : MonoBehaviour
+{
+    private Rigidbody2D rigidbody2D;
+    public float MovementSpeed;
+    //0-noMove,1-Up,2-Down,3-Left,4-Right TO-DO change for ENUM
+    public int direction;
+    public float Force;
+    private int lastKnownDirection;
+    private bool isMoving = false;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(isMoving == false)
+        {
+            direction = Random.Range(1, 4);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        MoveByForce(direction);
+    }
+
+    private void MoveByForce(int direction)
+    {
+        if (direction == 1 && lastKnownDirection!=1)
+        {
+            isMoving = true;
+            lastKnownDirection = direction;
+            rigidbody2D.velocity = new Vector2(0, 0);
+            rigidbody2D.AddForce(Vector2.up * Force);
+        }
+        if (direction == 2 && lastKnownDirection != 2)
+        {
+            isMoving = true;
+            lastKnownDirection = direction;
+            rigidbody2D.velocity = new Vector2(0, 0);
+            rigidbody2D.AddForce(Vector2.down * Force);
+        }
+        if (direction == 3 && lastKnownDirection != 3)
+        {
+            isMoving = true;
+            lastKnownDirection = direction;
+            rigidbody2D.velocity = new Vector2(0, 0);
+            rigidbody2D.AddForce(Vector2.left * Force);
+        }
+        if (direction == 4 && lastKnownDirection != 4)
+        {
+            isMoving = true;
+            lastKnownDirection = direction;
+            rigidbody2D.velocity = new Vector2(0, 0);
+            rigidbody2D.AddForce(Vector2.right * Force);
+        }
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "ChangePoint")
+        {
+            Destroy(collision.gameObject);
+        }
+        if(collision.tag == "Wall")
+        {
+            isMoving = false;
+            lastKnownDirection = direction;
+            Debug.Log(lastKnownDirection);
+            rigidbody2D.velocity = new Vector2(0,0);
+        }
+    }
+}
